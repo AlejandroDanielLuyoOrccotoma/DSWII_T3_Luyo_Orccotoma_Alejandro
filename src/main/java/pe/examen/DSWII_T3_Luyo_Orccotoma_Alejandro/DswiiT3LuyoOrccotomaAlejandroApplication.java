0795b2e955b9fa21ac712306b2f191782a60efd5
MadAlexO <aluyo_2012@hotmail.com>
2024-07-02 15:23:09 -0500

    Primera subida

---
+++ src/main/java/pe/examen/DSWII_T3_Luyo_Orccotoma_Alejandro/DswiiT3LuyoOrccotomaAlejandroApplication.java
@@ -0,0 +1,13 @@
+package pe.examen.DSWII_T3_Luyo_Orccotoma_Alejandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3LuyoOrccotomaAlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3LuyoOrccotomaAlejandroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/examen/DSWII_T3_Luyo_Orccotoma_Alejandro/DswiiT3LuyoOrccotomaAlejandroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.examen.DSWII_T3_Luyo_Orccotoma_Alejandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3LuyoOrccotomaAlejandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
